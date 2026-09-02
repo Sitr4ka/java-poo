9bfdd4e2553694cd6ce95bdfa5ee15c35a1f5c02
Sitraka Fifaliana <fifasitrakarkt@gmail.com>
2026-09-02 06:40:39 +0300

    add our first affichage function with parameter

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        affichage("Sitraka");
+    }
+
+    public static void affichage(final String name) {
+        System.out.println("Bonjour " + name);
+    }
+}
